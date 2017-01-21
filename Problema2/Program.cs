4c0ec94782072669c0e6a574b47e330eeff28dc2
DESKTOP-C8I5QD1\raidy <raidy@outlook.es>
2017-01-21 13:34:23 -0400

    Add project files.

---
+++ Problema2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
